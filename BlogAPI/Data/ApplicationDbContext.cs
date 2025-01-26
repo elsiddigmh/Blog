2fe9850a686fc9a8eb5005e8738750dbfed2dfa6
tchclaravel <tchc100@gmail.com>
2025-01-26 22:27:44 +0300

    Add tables to database

--- BlogAPI/Data/ApplicationDbContext.cs
+++ BlogAPI/Data/ApplicationDbContext.cs
@@ -1,10 +1,16 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using BlogAPI.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace BlogAPI.Data
 {
     public class ApplicationDbContext : DbContext
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base (options) { }
+
+        public DbSet<Post> Posts { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Comment> Comments { get; set; }
 
 
 
