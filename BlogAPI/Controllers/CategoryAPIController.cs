d542fb248e7010263adbd4c3afc25c9a2aa6d862
tchclaravel <tchc100@gmail.com>
2025-01-29 16:30:08 +0300

    Create APIReponse Scheme

---
+++ BlogAPI/Controllers/CategoryAPIController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BlogAPI.Controllers
+{
+
+    [Route("api/CategoryAPI")]
+    [ApiController]
+    public class CategoryAPIController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ BlogAPI/Models/APIResponse.cs
@@ -0,0 +1,18 @@
+﻿using System.Net;
+
+namespace BlogAPI.Models
+{
+    public class APIResponse
+    {
+        public HttpStatusCode StatusCode {  get; set; }
+        public bool IsSuccess { get; set; }
+        public object Result { get; set; }
+        public List<string> ErrorMessages { get; set; }
+
+
+        public APIResponse() { 
+            ErrorMessages = new List<string>();
+        }
+
+    }
+}
