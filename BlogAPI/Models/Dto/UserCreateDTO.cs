bb1696774d0efde42d0b091b70e54323373c1370
tchclaravel <tchc100@gmail.com>
2025-02-01 22:11:35 +0300

    Create User DTOs

---
+++ BlogAPI/Models/Dto/UserCreateDTO.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlogAPI.Models.Dto
+{
+    public class UserCreateDTO
+    {
+        [Required]
+        [MaxLength(20)]
+        public string UserName { get; set; }
+        [Required]
+        [MaxLength(30)]
+        public string Email { get; set; }
+        public string HashPassword { get; set; }
+        public string Role { get; set; }
+    }
+}
---
+++ BlogAPI/Models/Dto/UserDTO.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlogAPI.Models.Dto
+{
+    public class UserDTO
+    {
+        [Required]
+        [MaxLength(20)]
+        public string UserName { get; set; }
+        [Required]
+        [MaxLength(30)]
+        public string Email { get; set; }
+        public string HashPassword { get; set; }
+        public string Role { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+
+        // Relationships
+        public ICollection<Post> Posts { get; set; } // One-to-Many
+        public ICollection<Comment> Comments { get; set; } // One-to-Many
+    }
+
+}
