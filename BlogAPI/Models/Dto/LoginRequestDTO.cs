938b6245a681e989246dca15449bc621d93a256a
tchclaravel <tchc100@gmail.com>
2025-02-03 16:25:54 +0300

    Create Authentication DTOs

---
+++ BlogAPI/Models/Dto/LoginRequestDTO.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlogAPI.Models.Dto
+{
+    public class LoginRequestDTO
+    {
+        [Required]
+        [MaxLength(20)]
+        public string UserName { get; set; }
+        [Required]
+        public string HashPassword { get; set; }
+    }
+}
---
+++ BlogAPI/Models/Dto/LoginResponseDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace BlogAPI.Models.Dto
+{
+    public class LoginResponseDTO
+    {
+        public User User { get; set; }
+        public string Token { get; set; }
+
+    }
+}
