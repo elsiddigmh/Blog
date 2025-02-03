3b550b8cc8051dae8b1546977f4754a7a18f199d
tchclaravel <tchc100@gmail.com>
2025-02-03 17:02:12 +0300

    Add authentication methods in repository

--- BlogAPI/Repository/IRepository/IUserRepository.cs
+++ BlogAPI/Repository/IRepository/IUserRepository.cs
@@ -1,9 +1,12 @@
 ﻿using BlogAPI.Models;
+using BlogAPI.Models.Dto;
 
 namespace BlogAPI.Repository.IRepository
 {
     public interface IUserRepository : IRepository<User>
     {
+        public bool IsUiniqueUser(string  username);
+        public Task<LoginResponseDTO> Login(LoginRequestDTO loginRequestDTO);
         Task<User> UpdateAsync(User entity);
     }
 }
--- BlogAPI/Repository/UserRepository.cs
+++ BlogAPI/Repository/UserRepository.cs
@@ -1,15 +1,70 @@
 ﻿using BlogAPI.Data;
 using BlogAPI.Models;
+using BlogAPI.Models.Dto;
 using BlogAPI.Repository.IRepository;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Runtime.InteropServices;
+using System.Security.Claims;
+using System.Text;
 
 namespace BlogAPI.Repository
 {
     public class UserRepository : Repository<User>, IUserRepository
     {
         private readonly ApplicationDbContext _context;
-        public UserRepository(ApplicationDbContext context) : base(context) 
+        private string _secretKey;
+        public UserRepository(ApplicationDbContext context, IConfiguration configuration) : base(context) 
         {
             _context = context;
+            _secretKey = configuration.GetValue<string>("ApiSettings:Secret");
+        }
+
+        public bool IsUiniqueUser(string username)
+        {
+            var user = _context.Users.FirstOrDefault(u => u.UserName == username);
+
+            if (user == null) {
+                return true;
+            }
+
+            return false;
+        }
+
+        public async Task<LoginResponseDTO> Login(LoginRequestDTO loginRequestDTO)
+        {
+            var user = _context.Users.FirstOrDefault(u=> u.UserName == loginRequestDTO.UserName && u.HashPassword == loginRequestDTO.HashPassword);
+
+            if (user == null) {
+                return new LoginResponseDTO()
+                {
+                    User = null,
+                    Token = ""
+                };
+            }
+
+            // IF user found => Generate JWT Token
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var key = Encoding.ASCII.GetBytes(_secretKey);
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(new Claim[]
+                {
+                    new Claim(ClaimTypes.Name, user.Id.ToString()),
+                    new Claim(ClaimTypes.Role, user.Role)
+                }),
+                Expires = DateTime.UtcNow.AddDays(7),
+                SigningCredentials = new(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+            };
+
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+            LoginResponseDTO loginResponseDTO = new LoginResponseDTO()
+            {
+                Token = tokenHandler.WriteToken(token),
+                User = user
+            };
+
+            return loginResponseDTO;
         }
 
         public async Task<User> UpdateAsync(User entity)
