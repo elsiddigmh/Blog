c107d2235aa9de831000e857317372cec209cb9d
tchclaravel <tchc100@gmail.com>
2025-02-04 22:50:14 +0300

    Add IBaseService and implement it in BaseService

---
+++ BlogWeb/Services/IServices/IBaseService.cs
@@ -0,0 +1,10 @@
+﻿using BlogWeb.Models;
+
+namespace BlogWeb.Services.IServices
+{
+    public interface IBaseService
+    {
+        public APIResponse responseModel { get; set; }
+        public Task<T> SendAsync<T>(APIRequest apiRequest);
+    }
+}
