7f9a5a79a071c9feb1647382757a3d9d18af0d47
tchclaravel <tchc100@gmail.com>
2025-02-04 19:00:18 +0300

    Create APIRequest Model

---
+++ BlogWeb/Models/APIRequest.cs
@@ -0,0 +1,12 @@
+﻿using static BlogUtility.SD;
+
+namespace BlogWeb.Models
+{
+    public class APIRequest
+    {
+        public ApiType ApiType { get; set; } = ApiType.GET;
+        public string Url { get; set; }
+        public object Data { get; set; }
+        public string Token { get; set; }
+    }
+}
