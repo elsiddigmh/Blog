a6a9f1b196383dd662e6fbcd55e4d4a313ded9d3
tchclaravel <tchc100@gmail.com>
2025-02-04 18:55:50 +0300

    Add some static data in indvidual class library

---
+++ BlogUtility/SD.cs
@@ -0,0 +1,17 @@
+﻿namespace BlogUtility
+{
+    public static class SD
+    {
+
+        public enum ApiType
+        {
+            GET,
+            POST,
+            PUT,
+            DELETE
+        }
+
+        public static string SessionToken = "JwtToken";
+
+    }
+}
