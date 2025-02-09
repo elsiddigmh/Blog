c551ff77b0e9f15ba7a6d258a78b7061889f00c9
tchclaravel <tchc100@gmail.com>
2025-02-09 18:33:49 +0300

    Add Register & Login View

---
+++ BlogWeb/Controllers/AuthController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BlogWeb.Controllers
+{
+	public class AuthController : Controller
+	{
+		public IActionResult Index()
+		{
+			return View();
+		}
+
+	}
+}
--- BlogWeb/Controllers/HomeController.cs
+++ BlogWeb/Controllers/HomeController.cs
@@ -18,15 +18,23 @@ namespace BlogWeb.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
+		[HttpGet]
+		[Route("Register")]
+		public IActionResult Register()
+		{
+			return View();
+		}
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
-    }
+
+
+
+		[HttpGet]
+		[Route("Login")]
+		public IActionResult Login()
+		{
+			return View();
+		}
+
+
+	}
 }
