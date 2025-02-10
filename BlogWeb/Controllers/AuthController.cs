dcaa63fc1a14bcef1c4fd1017735afacc67df9e3
tchclaravel <tchc100@gmail.com>
2025-02-10 11:09:38 +0300

    implement register action

--- BlogWeb/Controllers/AuthController.cs
+++ BlogWeb/Controllers/AuthController.cs
@@ -1,14 +1,44 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AutoMapper;
+using BlogWeb.Models;
+using BlogWeb.Models.Dto;
+using BlogWeb.Services.IServices;
+using Microsoft.AspNetCore.Mvc;
 
 namespace BlogWeb.Controllers
 {
 	public class AuthController : Controller
 	{
+		private readonly IUserService _userService;
+		private readonly IMapper _mapper;
+		public AuthController(IUserService userService, IMapper mapper)
+		{
+			_userService = userService;
+			_mapper = mapper;
+		}
+
 		[HttpGet]
 		[Route("Register")]
 		public IActionResult Register()
 		{
 			return View();
+		}
+
+		[HttpPost]
+		[Route("Register")]
+		public async Task<IActionResult> Register(UserCreateDTO userDTO)
+		{
+			if (ModelState.IsValid)
+			{
+				var response = await _userService.CreateAsync<APIResponse>(userDTO);
+				if (response != null && response.IsSuccess) {
+					TempData["success"] = "Your account has been created!";
+					return RedirectToAction(nameof(Login));
+				}
+			}
+
+			TempData["error"] = "Something went wrong!";
+			return View(userDTO);
+
 		}
 
 
--- BlogWeb/Models/Dto/UserCreateDTO.cs
+++ BlogWeb/Models/Dto/UserCreateDTO.cs
@@ -13,7 +13,7 @@ namespace BlogWeb.Models.Dto
         [MaxLength(30)]
         public string Email { get; set; }
         public string HashPassword { get; set; }
-        public string Role { get; set; }
+        public string Role { get; set; } = "author";
 
     }
 
