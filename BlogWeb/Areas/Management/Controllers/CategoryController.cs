9ca5976feadf96d7906d3b8391d7456fe65d23d7
tchclaravel <tchc100@gmail.com>
2025-02-15 12:12:03 +0300

    Create Category Controller

---
+++ BlogWeb/Areas/Management/Controllers/CategoryController.cs
@@ -0,0 +1,22 @@
+﻿using AutoMapper;
+using BlogWeb.Services.IServices;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlogWeb.Areas.Management.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ICategoryService _categoryService;
+        private readonly IMapper _mapper;
+
+        public CategoryController(ICategoryService categoryService, IMapper mapper)
+        {
+            _categoryService = categoryService;
+            _mapper = mapper;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
