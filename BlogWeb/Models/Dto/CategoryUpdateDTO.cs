3dff855dc33c8a0a45d1c0af1b3cfbf6541aa929
tchclaravel <tchc100@gmail.com>
2025-02-15 12:06:23 +0300

    Implement Category Service

--- BlogWeb/Models/Dto/CategoryUpdateDTO.cs
+++ BlogWeb/Models/Dto/CategoryUpdateDTO.cs
@@ -4,6 +4,7 @@ namespace BlogWeb.Models.Dto
 {
     public class CategoryUpdateDTO
     {
+        public int Id { get; set; }
         [Required]
         [MaxLength(20)]
         public string Name { get; set; }
---
+++ BlogWeb/Services/CategoryService.cs
@@ -0,0 +1,65 @@
+﻿using BlogUtility;
+using BlogWeb.Models;
+using BlogWeb.Models.Dto;
+using BlogWeb.Services.IServices;
+
+namespace BlogWeb.Services
+{
+    public class CategoryService : BaseService, ICategoryService
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+        private readonly string _appUrl;
+
+        public CategoryService(IHttpClientFactory httpClientFactory, IConfiguration configuration) : base(httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+            _appUrl = configuration.GetValue<string>("ServiceUrls:BlogAPI");
+        }
+        public Task<T> CreateAsync<T>(CategoryCreateDTO categoryDTO)
+        {
+            return SendAsync<T>(new APIRequest
+            {
+                ApiType = SD.ApiType.POST,
+                Data = categoryDTO,
+                Url = _appUrl + "/api/categoryAPI"
+            });
+        }
+
+        public Task<T> DeleteAsync<T>(int id)
+        {
+            return SendAsync<T>(new APIRequest
+            {
+                ApiType = SD.ApiType.DELETE,
+                Url = _appUrl + "/api/categoryAPI/" + id
+            });
+        }
+
+        public Task<T> GetAllAsync<T>()
+        {
+            return SendAsync<T>(new APIRequest
+            {
+                ApiType = SD.ApiType.GET,
+                Url = _appUrl + "/api/categoryAPI"
+            });
+        }
+
+        public Task<T> GetAsync<T>(int id)
+        {
+            return SendAsync<T>(new APIRequest
+            {
+                ApiType = SD.ApiType.GET,
+                Url = _appUrl + "/api/categoryAPI/" + id
+            });
+        }
+
+        public Task<T> UpdateAsync<T>(CategoryUpdateDTO categoryDTO)
+        {
+            return SendAsync<T>(new APIRequest
+            {
+                ApiType = SD.ApiType.PUT,
+                Data = categoryDTO,
+                Url = _appUrl + "/api/categoryAPI/" + categoryDTO.Id
+            });
+        }
+    }
+}
