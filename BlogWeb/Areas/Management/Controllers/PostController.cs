ff7c8843ac304ff193a2c944b9bdaa294063efc9
tchclaravel <tchc100@gmail.com>
2025-02-16 10:47:40 +0300

    Create Post Controller

---
+++ BlogWeb/Areas/Management/Controllers/PostController.cs
@@ -0,0 +1,36 @@
+﻿using AutoMapper;
+using BlogWeb.Models;
+using BlogWeb.Models.Dto;
+using BlogWeb.Services;
+using BlogWeb.Services.IServices;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace BlogWeb.Areas.Management.Controllers
+{
+    public class PostController : Controller
+    {
+        private readonly IPostService _postService;
+        private readonly IMapper _mapper;
+        public PostController(IPostService postService, IMapper mapper)
+        {
+            _postService = postService;
+            _mapper = mapper;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            APIResponse response = await _postService.GetAllAsync<APIResponse>();
+            var posts = JsonConvert.DeserializeObject<List<PostDTO>>(Convert.ToString(response.Result));
+            return View(posts);
+        }
+
+        public async Task<IActionResult> Create()
+        {
+            return View();
+        }
+
+
+
+    }
+}
