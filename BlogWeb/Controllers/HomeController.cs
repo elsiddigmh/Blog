9c5d9f5668b9435c3e7af949e8ad88a6c4dd1d08
tchclaravel <tchc100@gmail.com>
2025-04-19 17:10:08 +0300

    Display posts in main page

--- BlogWeb/Controllers/HomeController.cs
+++ BlogWeb/Controllers/HomeController.cs
@@ -1,5 +1,11 @@
 using BlogWeb.Models;
+using BlogWeb.Models.Dto;
+using BlogWeb.Services;
+using BlogWeb.Services.IServices;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Configuration;
 using System.Diagnostics;
 
 namespace BlogWeb.Controllers
@@ -7,14 +13,38 @@ namespace BlogWeb.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly ICategoryService _categoryService;
+        private readonly IPostService _postService;
+        private readonly IHttpContextAccessor _httpContextAccessor;
+        private readonly string _token;
+        private readonly string _baseUrl;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, IConfiguration configuration, ICategoryService categoryService, IPostService postService, IHttpContextAccessor httpContextAccessor)
         {
             _logger = logger;
+            _baseUrl = configuration.GetValue<string>("ServiceUrls:BlogAPI");
+            _categoryService = categoryService;
+            _postService = postService;
+            _httpContextAccessor = httpContextAccessor;
+            _token = _httpContextAccessor.HttpContext?.Request.Cookies["AuthToken"];
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
+            var categoryResponse = await _categoryService.GetAllAsync<APIResponse>();
+            var categories = JsonConvert.DeserializeObject<List<CategoryDTO>>(Convert.ToString(categoryResponse.Result));
+
+            var postResponse = await _postService.GetAllAsync<APIResponse>();
+            var posts = JsonConvert.DeserializeObject<List<PostDTO>>(Convert.ToString(postResponse.Result));
+
+            var latestOne = posts.OrderByDescending(p => p.Id).FirstOrDefault();
+            var latestPosts = posts.OrderByDescending(p => p.Id).Take(5).ToList();
+
+
+            ViewData["latestOne"] = latestOne;
+            ViewData["latestPosts"] = latestPosts;
+            ViewData["baseUrl"] = _baseUrl;
+
             return View();
         }
 
---
+++ BlogWeb/Helpers/HtmlHelpers.cs
@@ -0,0 +1,16 @@
+﻿using System.Text.RegularExpressions;
+
+namespace BlogWeb.Healpers
+{
+    public static class HtmlHelpers
+    {
+        public static string StripHtml(string input) { 
+            if(string.IsNullOrEmpty(input)) 
+                return string.Empty;
+
+            return Regex.Replace(input, "<.*?>", string.Empty);
+            
+        }
+
+    }
+}
